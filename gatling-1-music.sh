21fd9de596c8594396e7ae81b8f710518806d2e4
tst <shitaot@sfu.ca>
2022-03-30 14:25:52 -0700

    service initial load

---
+++ gatling-1-music.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+docker container run --rm \
+  -v ${PWD}/gatling/results:/opt/gatling/results \
+  -v ${PWD}/gatling:/opt/gatling/user-files \
+  -v ${PWD}/gatling/target:/opt/gatling/target \
+  -e CLUSTER_IP=a0f10e5ab1efc44aa9449f0d56b97cd5-662372185.us-west-2.elb.amazonaws.com \
+  -e USERS=1 \
+  -e SIM_NAME=ReadMusicSim \
+  --label gatling \
+  ghcr.io/scp-2021-jan-cmpt-756/gatling:3.4.2 \
+  -s proj756.ReadMusicSim
