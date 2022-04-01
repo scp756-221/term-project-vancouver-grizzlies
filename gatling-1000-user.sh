43ccc2e9c8eed362709e197697793a5a69816733
hajimexu <xuxiangx@sfu.ca>
2022-04-01 16:50:58 -0700

    gatling 500 to 1000

--- gatling-1000-user.sh
+++ gatling-1000-user.sh
@@ -4,8 +4,8 @@ docker container run --rm \
   -v ${PWD}/gatling:/opt/gatling/user-files \
   -v ${PWD}/gatling/target:/opt/gatling/target \
   -e CLUSTER_IP=a0f10e5ab1efc44aa9449f0d56b97cd5-662372185.us-west-2.elb.amazonaws.com \
-  -e USERS=500 \
-  -e SIM_NAME=ReadMusicSim \
+  -e USERS=1000 \
+  -e SIM_NAME=ReadUserSim \
   --label gatling \
   ghcr.io/scp-2021-jan-cmpt-756/gatling:3.4.2 \
-  -s proj756.ReadMusicSim
+  -s proj756.ReadUserSim
--- gatling-set-ip.sh
+++ gatling-set-ip.sh
@@ -8,6 +8,6 @@ sed -i "s/CLUSTER_IP=.*/CLUSTER_IP=$1 \\\/g" gatling-10-music.sh
 sed -i "s/CLUSTER_IP=.*/CLUSTER_IP=$1 \\\/g" gatling-10-playlist.sh
 sed -i "s/CLUSTER_IP=.*/CLUSTER_IP=$1 \\\/g" gatling-10-user.sh
 
-sed -i "s/CLUSTER_IP=.*/CLUSTER_IP=$1 \\\/g" gatling-500-music.sh
-sed -i "s/CLUSTER_IP=.*/CLUSTER_IP=$1 \\\/g" gatling-500-playlist.sh
-sed -i "s/CLUSTER_IP=.*/CLUSTER_IP=$1 \\\/g" gatling-500-user.sh
+sed -i "s/CLUSTER_IP=.*/CLUSTER_IP=$1 \\\/g" gatling-1000-music.sh
+sed -i "s/CLUSTER_IP=.*/CLUSTER_IP=$1 \\\/g" gatling-1000-playlist.sh
+sed -i "s/CLUSTER_IP=.*/CLUSTER_IP=$1 \\\/g" gatling-1000-user.sh
